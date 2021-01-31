a4b01601b141b25157db3c956fc575589d8d24a0
jwilliams2019 <jwilliams19@mail.wou.edu>
2021-01-31 00:00:40 -0800

    Removes wolfit files
    The wolfit folder, which has been copied elsewhere,
    is not necessary for turning in the assignment.

--- rundev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export WOLFIT_SETTINGS=$(pwd)/dev.settings
-flask run --host=0.0.0.0
--- runtests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export WOLFIT_SETTINGS=$(pwd)/test.settings
-export FLASK_ENV=test
-export FLASK_DEBUG=0
-pytest -W ignore::DeprecationWarning -x $@
